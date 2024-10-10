40857bca9cb0f90f535ad0abd658707c3949c9f9
Lucas <lucasludovicocabraal@gmail.com>
2024-10-10 09:55:16 -0300

    ajustes

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from "react"
+
 const Home =()=>{
+
+    const [listaProduto, setListaProduto]=useState([])
+
+    useEffect(()=>{
+        fetch("http://localhost:5000/produto")
+        .then((res)=>{
+            return res.json()
+        })
+        .then((data)=>{
+            setListaProduto(data)
+        })
+    })
+
     return(
         <>
-        <h1>home</h1>
+            {listaProduto.map((item, index)=>(
+                <div key={index}>
+                    {item.nome}
+                    <img src={item.foto}/>
+
+                </div>
+            ))
+                
+            }
         </>
     )
 }
